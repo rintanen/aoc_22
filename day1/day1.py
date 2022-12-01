5e098ad1fe837c433f4b5a71b6c5186217cff36b
rintanen <perttu.rintanen@ouraring.com>
2022-12-01 13:11:33 +0200

    day1

---
+++ day1/day1.py
@@ -0,0 +1,32 @@
+from InputBase import InputBase
+from functools import cached_property
+
+
+class Input(InputBase):
+    def read_raw_input(self):
+        input_list = ",".join(self.raw_input).split(',\n,')
+        items = [[int(item) for item in one_elf_items.split(',')] for one_elf_items in input_list]
+        return items
+
+
+class FoodItems:
+    def __init__(self, input_path):
+        self.input = Input(input_path)
+
+    @cached_property
+    def items(self):
+        return self.input.read_raw_input()
+
+    @property
+    def total_calories(self):
+        return [sum(items) for items in self.items]
+
+
+if __name__ == '__main__':
+    food_items = FoodItems('input.txt')
+    print(f'PART 1: {max(food_items.total_calories)}')
+
+    max_three = sorted(food_items.total_calories, reverse=True)[:3]
+    print(f'PART 2: {sum(max_three)}')
+
+

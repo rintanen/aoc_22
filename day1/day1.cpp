0bf0441b3dafab07a138b4174035ad11504dede6
rintanen <perttu.rintanen@ouraring.com>
2022-12-03 20:15:37 +0200

    day1 cpp

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,10 +4,11 @@
 #include <fstream>
 #include <algorithm>
 
+
 int main()
 {   
     std::ifstream raw_input("input.txt");
-    std::vector<uint> calories;
+    std::vector<uint32_t> calories;
     std::string line;
 
     uint32_t this_elf_calories = 0;
@@ -25,6 +26,18 @@ int main()
     uint32_t max_calories = *std::max_element(calories.begin(), calories.end());
 
     std::cout << "PART 1: " << max_calories << std::endl;
+
+    std::sort(calories.begin(), calories.end());
+
+    std::vector<uint32_t> top_three(calories.end() - 3, calories.end());
+
+    uint32_t top_three_sum = 0;
+    for (int item: top_three)
+    {
+        top_three_sum += item;
+    }
+
+    std::cout << "PART 2: " << top_three_sum << std::endl;
 
     raw_input.close();
     

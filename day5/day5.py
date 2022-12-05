c1c49233a93363956a39f9a67900b50f582f881a
rintanen <perttu.rintanen@ouraring.com>
2022-12-05 11:45:52 +0200

    day5 py

---
+++ day5/day5.py
@@ -0,0 +1,43 @@
+from InputBase import InputBase
+import re
+
+class Input(InputBase):
+    def read_task_input(self) -> list:
+        txt = "".join(self.raw_input)
+        return self.stacks(txt), self.rearrangements(txt)
+
+    def rearrangements(self, txt):
+        txt = txt.split('\nmove')[1:]
+        rearrangements = [re.findall(r'\d+', rearrangement) for rearrangement in txt]
+        return [[int(s) for s in num_str] for num_str in rearrangements]
+
+    def stacks(self, txt):
+        txt = txt.split('\n\n')[0].split('\n')
+        stacks = txt[:-1]
+        crate_indices = range(1, len(max(stacks, key=len)), 4)
+        new_stacks = []
+        for i in crate_indices:
+            new_stack = ''
+            for stack in stacks:
+                if len(stack) < i:
+                    continue
+                new_stack += stack[i]
+            new_stacks.append(new_stack.strip())
+        return new_stacks
+
+
+def run(stacks, rearrangements, part):
+    for move_this, from_this, to_this in rearrangements:
+        if part == 1:
+            stacks[to_this - 1] = "".join(reversed(stacks[from_this - 1][:move_this])) + stacks[to_this - 1]
+        elif part == 2:
+            stacks[to_this - 1] = stacks[from_this - 1][:move_this] + stacks[to_this - 1]
+        stacks[from_this - 1] = stacks[from_this - 1][move_this:]
+    return "".join([s[0] for s in stacks])
+
+
+if __name__ == '__main__':
+    stacks, rearrangements = Input('input.txt').read_task_input()
+
+    print(f'PART 1: {run(stacks.copy(), rearrangements, 1)}')
+    print(f'PART 2: {run(stacks.copy(), rearrangements, 2)}')

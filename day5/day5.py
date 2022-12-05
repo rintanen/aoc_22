a7829a0c6769c828306563adaeb1facea43a5bee
rintanen <perttu.rintanen@ouraring.com>
2022-12-05 11:49:21 +0200

    type hints

--- day5/day5.py
+++ day5/day5.py
@@ -2,16 +2,16 @@ from InputBase import InputBase
 import re
 
 class Input(InputBase):
-    def read_task_input(self) -> list:
+    def read_task_input(self) -> tuple[list[str], list[int]]:
         txt = "".join(self.raw_input)
         return self.stacks(txt), self.rearrangements(txt)
 
-    def rearrangements(self, txt):
+    def rearrangements(self, txt: str) -> list[int]:
         txt = txt.split('\nmove')[1:]
         rearrangements = [re.findall(r'\d+', rearrangement) for rearrangement in txt]
         return [[int(s) for s in num_str] for num_str in rearrangements]
 
-    def stacks(self, txt):
+    def stacks(self, txt: str) -> list[str]:
         txt = txt.split('\n\n')[0].split('\n')
         stacks = txt[:-1]
         crate_indices = range(1, len(max(stacks, key=len)), 4)
@@ -26,7 +26,7 @@ class Input(InputBase):
         return new_stacks
 
 
-def run(stacks, rearrangements, part):
+def run(stacks: list[str], rearrangements: list[int], part: int) -> str:
     for move_this, from_this, to_this in rearrangements:
         if part == 1:
             stacks[to_this - 1] = "".join(reversed(stacks[from_this - 1][:move_this])) + stacks[to_this - 1]

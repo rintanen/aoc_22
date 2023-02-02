bd6ac121482e52e4aaa3fde93b62b704341edd38
rintanen <perttu.rintanen@ouraring.com>
2023-02-02 10:26:20 +0200

    fars

--- day4/day4.rs
+++ day4/day4.rs
@@ -21,28 +21,28 @@ fn main() {
         .count();
     
     let pt2 = task_input
-    .lines()
-    .map(|l| {
-        let (first, second) = l.split_once(',').unwrap();
-        let ((a, b), (c, d)) = (first.split_once('-').unwrap(), second.split_once('-').unwrap());
-        (
-            a.parse::<u32>().unwrap(),
-            b.parse::<u32>().unwrap(),
-            c.parse::<u32>().unwrap(),
-            d.parse::<u32>().unwrap()
-        )})
-    .filter_map(|(a, b, c, d)| {
-        let first = a..b + 1;
-        let second = c..d + 1;
-
-        for i in first {
-            if second.contains(&i) {
-                return Some(true);
-            } 
-        }
-        None
-    })
-    .count();
+        .lines()
+        .map(|l| {
+            let (first, second) = l.split_once(',').unwrap();
+            let ((a, b), (c, d)) = (first.split_once('-').unwrap(), second.split_once('-').unwrap());
+            (
+                a.parse::<u32>().unwrap(),
+                b.parse::<u32>().unwrap(),
+                c.parse::<u32>().unwrap(),
+                d.parse::<u32>().unwrap()
+            )})
+        .filter_map(|(a, b, c, d)| {
+            let first = a..b + 1;
+            let second = c..d + 1;
+            
+            for i in first {
+                if second.contains(&i) {
+                    return Some(true);
+                } 
+            }
+            None
+        })
+        .count();
 
     println!("{}", pt1);
     println!("{}", pt2);
